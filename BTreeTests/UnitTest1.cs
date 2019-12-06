f4cae2b0736fab0f7e91247a8a773b16131d4fa5
Halloween <zumik12@yandex.ru>
2019-12-06 20:49:48 +0300

    Create BTree
    Добавлены значения и список ссылок на детей у узла

--- BTreeTests/UnitTest1.cs
+++ BTreeTests/UnitTest1.cs
@@ -13,6 +13,10 @@ namespace Tests
           Assert.True(tree != null);
           Assert.AreEqual(3, tree.Degree);
           Assert.AreEqual(1, tree.Height);
+          Assert.AreEqual(0, tree.Root.Entries.Count);
+          Assert.AreEqual(0, tree.Root.Children.Count);
+          Assert.NotNull(tree.Root.Entries);
+          Assert.NotNull(tree.Root.Children);
           Assert.True(tree.Root != null);
         }
         
--- TestPS3/Node.cs
+++ TestPS3/Node.cs
@@ -1,13 +1,19 @@
 
+using System.Collections.Generic;
+
 namespace TestPS3
 {
     public class Node<TK, TP>
     {
         private int _degree;
-        
+        public List<Node<TK, TP>> Children { get; set; }
+        public List<Entry<TK, TP>> Entries { get; set; }
+
         public Node(int degree)
         {
             this._degree = _degree;
+            Children = new List<Node<TK, TP>>();
+            Entries = new List<Entry<TK, TP>>();
         }
     }
 }
